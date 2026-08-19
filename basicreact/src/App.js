cc12015556f85efa29246df92ff3973ace35fa1d
Shahid Chaudhary <mailto:shahid_chaudhary@outlook.com>
2026-08-19 18:45:30 +0500

    when creating a component always  start functio name with uppercase and  good choice is to name  file name with uppercase

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -1,7 +1,11 @@
+import Chai from './Chai'
 function App() {
   return (
-   <h1>first React project |Alisha Arshad</h1>
+    <>
+    <Chai/>
+    <h1>first React project |Alisha Arshad</h1>
+    </>
+   
   );
 }
-
 export default App;
