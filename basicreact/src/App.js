b48acffec20df088c73ed556e3be5ad92e8ac5f9
Shahid Chaudhary <mailto:shahid_chaudhary@outlook.com>
2026-08-20 17:44:28 +0500

    build reaact app with functions

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -1,11 +1,16 @@
 import Chai from './Chai'
 function App() {
+  const Username = 'chai or react'
   return (
     <>
     <Chai/>
-    <h1>first React project |Alisha Arshad</h1>
+    <h1>first React project |Alisha Arshad  this is called expression {Username}</h1>
+    <p>A complete log of this run can be found in:
+    </p>
+    <p> we cant write expression like ifesle for loop in the expression hmm but we can write these in cont veriable and then declare that variable in the expression</p> 
     </>
    
   );
+
 }
 export default App;
--- basicreact/src/index.js
+++ basicreact/src/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+function myApp(){
+  return(
+    <>
+    <h1>Custom App</h1>
+    <p>A complete log of this run can be found in:
+    </p>
+    </>
+  )
+}
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+const anotherUser = " chai or react";
+const reactElement = React.createElement(
+  'a',//belly element
+  {href: 'https://google.com',target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+   // reactElement
+    
 );
 
